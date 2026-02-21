e65292bb5b2cf14a4c4c330683a4d17889972a85
AnyClaw <l.kolryser@yandex.ru>
2026-02-21 23:39:55 +0300

    first try

--- composeApp/src/androidMain/kotlin/com/github/anyclaw/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/github/anyclaw/MainActivity.kt
@@ -4,23 +4,40 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.github.anyclaw.routes.greetingRoute
+import com.github.anyclaw.routes.loginRoute
+import com.github.anyclaw.screens.GreetingScreen
+import com.github.anyclaw.screens.LoginScreen
 
-// changes
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
 
         setContent {
-            App()
+            val navController = rememberNavController()
+            NavHost(
+                navController = navController,
+                startDestination = greetingRoute
+            ){
+                composable(greetingRoute) {
+                    GreetingScreen({
+                        navController.graph.findNode(loginRoute)?.id?.let { resId ->
+                            navController.navigate(
+                                resId = resId,
+                            )
+                        }
+                    })
+                }
+
+                composable(loginRoute) {
+                    LoginScreen()
+                }
+            }
         }
     }
 }
 
-@Preview
-@Composable
-fun AppAndroidPreview() {
-    App()
-}
---
+++ composeApp/src/commonMain/kotlin/com/github/anyclaw/dimens/Dimens.kt
@@ -0,0 +1,6 @@
+package com.github.anyclaw.dimens
+
+import androidx.compose.ui.unit.dp
+
+val padding_32 = 32.dp
+val padding_64 = 64.dp
